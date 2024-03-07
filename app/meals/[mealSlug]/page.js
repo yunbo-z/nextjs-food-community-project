2d104d187a5f3f1a5744032469d1d12113ff6724
yunbo-z <zyunbo0330@gmail.com>
2024-03-07 22:07:46 +0100

    handle error and not-found page

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,8 +1,41 @@
-const MealDetails = (props) => {
-    return(
+import Image from 'next/image'
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css'
+import { notFound } from 'next/navigation';
+
+const MealDetails = ({ params }) => {
+    const meal = getMeal(params.mealSlug);
+
+    
+    if (!meal) {
+        notFound();//this will show the closes error.js or not-found.js
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g,'<br />')
+
+    return (
         <>
-            <h2>Here is page:{props.params.mealSlug}</h2>
-            <h2>Meal details</h2>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
+            </main>
         </>
     )
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,19 @@
+//suspense lets you display a fallback until its children have finished loading.
+import { Suspense } from "react"
+
 import Link from "next/link"
 import classes from "./page.module.css"
 import MealsGrid from "@/components/meals/meals-grid"
+import { getMeals } from "@/lib/meals"
 
 
-const MealsPage = () => {
+async function Meals() {
+    const meals = await getMeals()
+
+    return <MealsGrid meals={meals} />
+}
+
+function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -21,7 +31,9 @@ const MealsPage = () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={[]} />
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     )
